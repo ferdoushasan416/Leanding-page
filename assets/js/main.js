ddb04bc10feb537c76de55b2e8cb2cd3fbe8f605
Faisal <faisalevbhaluka@gmail.com>
2023-06-05 13:28:54 +0600

    now create menu background css style and tab click content image change html css or javasript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,33 @@
 (function ($) {
 "use strict";
 
-
 $(document).ready(function() {
     $('.nav-toggle').click(function(){
         $('ul').toggleClass('show');
     })
     
+});
+// Mobile Responsive////
+
+
+//Tab function //
+$('.invest-content-wrap').eq(0).addClass('active')
+$('.tab-image').hide()
+$('.tab-image').eq(0).show()
+
+$('.invest-content-wrap').each(function(i){
+    $(this).click(function(){
+        
+        if( $(this).hasClass('active') ) return false
+        
+        else{
+            $('.invest-content-wrap').removeClass('active')
+            $(this).addClass('active')
+            $('.tab-image').hide()
+            $('.tab-image').eq(i).show()
+            
+        }
+    })
 });
 
 
@@ -29,7 +50,8 @@ $('.slider-active').owlCarousel({
             items:1
         }
     }
-})
+});
+
 
 
 // WOW active
